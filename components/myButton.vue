ef2cff2ac945ae1e9c35b97807adda1e92709c6b
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-26 13:00:11 +0100

    création d'un composant bouton

---
+++ components/myButton.vue
@@ -0,0 +1,64 @@
+
+<template>
+    <button class="myButton" :class="className">
+        <RouterLink :to="link" v-if="link">
+            <slot/>
+        </RouterLink>
+        
+        <a :href="href" v-else-if="href" target="_blank" rel="noopener noreferrer"><slot/></a>
+        
+        <span v-else><slot/></span>
+    </button>
+</template>
+
+<style lang="scss">
+.myButton{
+    cursor: pointer;
+
+    &.-t_button{
+        @include my_button;
+    
+        &:hover{
+            background: darken($c-main, 10%);
+        }
+    
+        &.-big{
+            padding: $pc-m-sm $pc-m-lg;
+            font-size: $pc-f-xl;
+        }
+    }
+
+    &.-t_link{
+        font-size: $pc-f-md;
+        font-weight: $fw-bold;
+        font-style: italic;
+        color: $c-main;
+        text-transform: uppercase;
+        cursor: pointer;
+    
+        &:hover{
+            color: darken($c-main, 10%);
+        }
+    
+        &.-small{
+            font-size: $pc-f-sm;
+        }
+    }
+}
+</style>
+
+<script setup>
+const props = defineProps({
+    link: String,
+    href: String,
+    type: String,
+    size: String,
+});
+
+const className = computed(() => ({
+    " -t_button": props.type === "t_button",
+    " -big": props.size === "big",
+    " -t_link": props.type === "t_link",
+    " -small": props.size === "small",
+}));
+</script>
