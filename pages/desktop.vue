6f5bc666724a74da58e2f6938dd49d1f7046c1d8
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-30 10:04:01 +0100

    ajustement style page bureau

--- pages/desktop.vue
+++ pages/desktop.vue
@@ -18,12 +18,12 @@
             </template>
 
             <section class="first_section">
-                <h2 class="global-titre_texte index__texte-title">Situation actuelle :</h2>
+                <h2 class="global-titre_texte desktop__texte-title">Situation actuelle :</h2>
                 <p class="index__texte-info">l'astronaute Spike Spiegel vient d'arriver sur la planète VMK_0324. Il est le seul survivant de son équipage.</p>
             </section>
             
             <section class="desktop__section">
-                <h2 class="global-titre_texte index__texte-title">Objectifs de la mission :</h2>
+                <h2 class="global-titre_texte desktop__texte-title">Objectifs de la mission :</h2>
                 <ul class="index__texte-info">
                     <li>récupérer des ressources pour survivre.</li>
                     <li>rencontrer et établir de bons rapport avec l'espèces intelligente local, les Bourpis.</li>
@@ -31,7 +31,9 @@
                 </ul>
             </section>
     
-            <myButton type="t_button" size="big" link="#">Commencer la mission</myButton>
+            <div class="desktop__button">
+                <myButton type="t_button" size="big" link="#">Commencer la mission</myButton>
+            </div>
         </NuxtLayout>
     </div>
 </template>
@@ -43,10 +45,24 @@
         margin: $ph-m-lg 0;
     }
 
+    &__texte-title{
+        margin-bottom: $ph-m-sm;
+        text-align: left;
+    }
+
+    &__button{
+        width: fit-content;
+        margin: auto;
+    }
+
     @include medium{
 
         &__section{
             margin: $pc-m-lg auto;
+        }
+
+        &__texte-title{
+            margin-bottom: $pc-m-sm;
         }
     }
 }
