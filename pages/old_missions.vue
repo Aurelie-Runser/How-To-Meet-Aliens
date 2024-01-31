d1dc892ee7a73a87fd2c8dc066d52ca2296ab36d
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-31 11:44:58 +0100

    ajout de l'affichage de toutes les fin débloqué, de tous les jours débloqué et de l'historique des missions

--- pages/old_missions.vue
+++ pages/old_missions.vue
@@ -16,16 +16,45 @@
                     <myButton type="t_link" size="small" link="/conditions_dutilisation">Conditions_d'utilisation_de_la_machine</myButton>
                 </div>
             </template>
-    
-            <ul>
-                <li v-for="j in historique.jours_debloques">{{ j.nom }}</li>
+
+            <div class="old__buttons">
+                <myButton type="t_button" @click="rapports(0)">Missions</myButton>
+                <myButton type="t_button" @click="rapports(1)">Jours</myButton>
+                <myButton type="t_button" @click="rapports(2)">Fins</myButton>
+            </div>
+
+            <h3>{{ titre }}</h3>
+
+            <ul v-if="missionsVisible">
+                <li v-for="m in missions" :key="m.date">
+                    {{ getMissionString(m) }}
+                </li>
             </ul>
+    
+            <ul v-else>
+                <li v-for="j in historique">{{ j.nom }}</li>
+            </ul>
+
         </NuxtLayout>
     </div>
 </template>
 
 <style lang="scss">
+.old{
 
+    &__buttons{
+        display: flex;
+        flex-wrap: wrap;
+        gap: $ph-m-lg;
+    }
+    
+    @include medium{
+        
+        &__buttons{
+            gap: $pc-m-lg;
+        }
+    }
+}
 </style>
   
 <script setup>
@@ -35,13 +64,45 @@ const store = useGlobalStore()
 
 const router = useRouter()
 
+const debloque = ref([])
+const missions = ref([])
 const historique = ref([])
+
+const missionsVisible = ref(true)
+const titre = ref("Rapports débloquées")
 
 // récupération de toutes les fins débloqué par l'user
 const getHistorique = async () => {
-    const response = await API.get(`/user/${store.token}/old_missions`)
-    historique.value = response.data
+    const response = await API.get(`/user/${store.token}/historique`)
+    missions.value = response.data.missions
+    debloque.value = response.data.rapports_debloques
+    historique.value = debloque.value
 }
+
+const rapports = async(r) => {
+    if (r == 0){
+        historique.value = missions.value
+        titre.value = "Missions passés"
+        missionsVisible.value = true
+    }
+    else if (r == 1){
+        // retourne tous les rapports
+        historique.value = debloque.value
+        titre.value = "Rapports débloquées"
+        missionsVisible.value = false
+    } else if (r == 2) {
+        // retourne seulement les fins
+        historique.value = debloque.value.filter(j => j.fin == 1)
+        titre.value = "Fins débloquées"
+        missionsVisible.value = false
+    }
+}
+
+const getMissionString = (m) => {
+  const date = m.date;
+  const parties = m.partie.map((jour) => jour.nom).join(' > ');
+  return `${date} : ${parties}`;
+};
 
 // suppression du token pour déconnecter l'utilisateur
 const deconnexion = async () => {
