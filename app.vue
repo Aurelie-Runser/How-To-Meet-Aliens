ee4c1393c179c13d45e209afa25f73c4844f6cad
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-02-09 11:45:35 +0100

    ajout du pseudo à pinia lors des rechargements

--- app.vue
+++ app.vue
@@ -10,8 +10,9 @@ const store = useGlobalStore()
 onMounted(() => {
     // fonction pour vérifier si l'utilisateur était connecté à sa dernière visite
     const token = localStorage.getItem("Token de Connexion")
+    const pseudo = localStorage.getItem("Pseudo")
     const mainColor = localStorage.getItem("Theme")
     const raportsDebloques = localStorage.getItem('Rapports Débloqués')
-    store.setUser(JSON.parse(token), mainColor, raportsDebloques)
+    store.setUser(JSON.parse(token), pseudo, mainColor, raportsDebloques)
 })
 </script>
